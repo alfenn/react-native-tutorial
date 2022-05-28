eb349b9da475e9db45fedd154c68c446dad411bd
alfenn <52635707+alfenn@users.noreply.github.com>
2022-05-28 13:38:50 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyle}>Welcome to my app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00f',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    color: '#fff',
+    fontSize: 32,
+  }
 });
